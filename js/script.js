15181e420fe4c2d6aebcdafa59ca1ebd3800bf28
zohra <zohrahanafi42@gmail.com>
2023-03-03 17:22:02 -0600

    v3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+var options = {
+    strings: ['Graduate student', 'Software Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  };
+  
+  var typed = new Typed('.titles', options);
